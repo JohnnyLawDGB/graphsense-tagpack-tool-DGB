8a45776c88e07e7ff1a000c7d9ae0033a1146aff
JohnnyLawDGB <johnnylaw2021@protonmail.com>
2025-07-21 00:27:25 +0000

    Add Digibyte support

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -1,4 +1,8 @@
-from tagpack.constants import suggest_networks_from_currency
+from tagpack.constants import (
+    suggest_networks_from_currency,
+    is_known_currency,
+    is_known_network,
+)
 
 
 def test_chain_suggestiong():
@@ -7,3 +11,8 @@ def test_chain_suggestiong():
     assert len(suggest_networks_from_currency("ETH")) == 0
 
     assert len(suggest_networks_from_currency("ETH", only_unknown=False)) == 1
+
+
+def test_dgb_known():
+    assert is_known_currency("DGB")
+    assert is_known_network("DGB")
